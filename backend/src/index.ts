bd6b1ec8607c97d5dbef710c44d24604540e81b0
Bíró Kálmán <linkee12@gmail.com>
2025-02-23 21:31:25 +0100

    refactor and cuple

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import { createBuilder, success, initRpc } from "@cuple/server";
+import { z } from "zod";
 import { getTablesData, TableName } from "./modell";
 
 dotenv.config();
 
 const app = express();
 const port = 3001;
+const builder = createBuilder(app);
 
-app.get("/admin/:tableName", async (req, res) => {
-  const alma = await getTablesData(req.params.tableName as TableName);
-  res.send(JSON.stringify(alma));
+const routes = {
+  list: builder
+    .querySchema(
+      z.object({
+        tableName: z.string(),
+      }),
+    )
+    .get(async ({ data }) => {
+      const rows = await getTablesData(data.query.tableName as TableName);
+      return success({
+        rows,
+      });
+    }),
+};
+
+initRpc(app, {
+  path: "/rpc",
+  routes,
 });
+
+export type Routes = typeof routes;
 
 app.listen(port, () => {
   console.log(`Server is running at http://0.0.0.0:${port}`);
--- backend/src/modell.ts
+++ backend/src/modell.ts
@@ -4,9 +4,10 @@ import { PrismaModels } from "prisma-models";
 export type TableName = keyof Models;
 type Models = PrismaModels<Prisma.ModelName, Prisma.TypeMap>;
 type OneOfTableNames = "categories";
+export type Row = Record<string, string | boolean | number> & { id: number };
 
 const prisma = new PrismaClient();
 
-export async function getTablesData(tableName: TableName) {
+export async function getTablesData(tableName: TableName): Promise<Row[]> {
   return await prisma[tableName as OneOfTableNames].findMany();
 }
